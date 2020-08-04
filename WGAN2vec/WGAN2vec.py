6bc918ff1523dbb8b2bd6c0ff3e6d52c7650ad9e
haka913 <43568065+haka913@users.noreply.github.com>
2020-08-04 22:05:39 +0900

    word2vec using WGAN

--- WGAN2vec/WGAN2vec.py
+++ WGAN2vec/WGAN2vec.py
@@ -1,13 +1,22 @@
 from __future__ import print_function, division
+# https://github.com/robert-d-schultz/gan-word-embedding
 
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.optimizers import Adam
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout
-from keras.layers import BatchNormalization, Activation, ZeroPadding2D
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import UpSampling2D, Conv2D
-from keras.models import Sequential, Model
-from keras.optimizers import RMSprop
+# from keras.layers import Input, Dense, Reshape, Flatten
+# from keras.layers import BatchNormalization, Activation
+# from keras.layers.advanced_activations import LeakyReLU
+# from keras.layers.convolutional import Conv2D, Conv2DTranspose
+# from keras.models import Sequential, Model
+
+# error occur!
+# from keras.optimizers import RMSprop
+# use instead
+from tensorflow.keras.optimizers import RMSprop
+
+from tensorflow.keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from tensorflow.keras.layers import BatchNormalization, Activation
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import UpSampling2D, Conv2D, Conv2DTranspose
+from tensorflow.keras.models import Sequential, Model
 
 import keras.backend as K
 
@@ -54,7 +63,7 @@ class WGAN2vec():
         self.img_rows = 7
         self.img_cols = 64
         self.channels = 1
-        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+        self.sentence_shape = (self.img_rows, self.img_cols, self.channels)
         self.latent_dim = 100
 
         # Following parameter and optimizer set as recommended in paper
@@ -73,13 +82,13 @@ class WGAN2vec():
 
         # The generator takes noise as input and generated imgs
         z = Input(shape=(self.latent_dim,))
-        img = self.generator(z)
+        sentence = self.generator(z)
 
         # For the combined model we will only train the generator
         self.critic.trainable = False
 
         # The critic takes generated images as input and determines validity
-        valid = self.critic(img)
+        valid = self.critic(sentence)
 
         # The combined model  (stacked generator and critic)
         self.combined = Model(z, valid)
@@ -94,21 +103,30 @@ class WGAN2vec():
 
         model = Sequential()
 
-        model.add(Dense(128 * 7 * 7, activation="relu", input_dim=self.latent_dim))
-        model.add(Reshape((7, 7, 128)))
-        model.add(UpSampling2D())
-        model.add(Conv2D(128, kernel_size=4, padding="same"))
+        # model.add(Dense(128 * 7 * 7, activation="relu", input_dim=self.latent_dim))
+        # model.add(Reshape((7, 7, 128)))
+        # model.add(UpSampling2D())
+        # model.add(Conv2D(128, kernel_size=4, padding="same"))
+        # model.add(BatchNormalization(momentum=0.8))
+        # model.add(Activation("relu"))
+        # model.add(UpSampling2D())
+        # model.add(Conv2D(64, kernel_size=4, padding="same"))
+        # model.add(BatchNormalization(momentum=0.8))
+        # model.add(Activation("relu"))
+        # model.add(Conv2D(self.channels, kernel_size=4, padding="same"))
+        # model.add(Activation("tanh"))
+
+        model.add(Dense(512, input_dim=self.latent_dim))
         model.add(BatchNormalization(momentum=0.8))
         model.add(Activation("relu"))
-        model.add(UpSampling2D())
-        model.add(Conv2D(64, kernel_size=4, padding="same"))
-        model.add(BatchNormalization(momentum=0.8))
+        model.add(Reshape((1, 1, 512)))
+        model.add(Conv2DTranspose(256, kernel_size=(3, 16), strides=2))
         model.add(Activation("relu"))
-        model.add(Conv2D(self.channels, kernel_size=4, padding="same"))
-        model.add(Activation("tanh"))
+        model.add(Conv2DTranspose(1, kernel_size=(3, 34), strides=2))
+        model.add(Reshape((7, 64, 1)))
 
         model.summary()
-        model.save("WGAN2vec_generator.h5")
+        # model.save("WGAN2vec_generator.h5")
 
         noise = Input(shape=(self.latent_dim,))
         img = model(noise)
@@ -119,36 +137,43 @@ class WGAN2vec():
 
         model = Sequential()
 
-        model.add(Conv2D(16, kernel_size=3, strides=2, input_shape=self.img_shape, padding="same"))
+        # model.add(Conv2D(16, kernel_size=3, strides=2, input_shape=self.img_shape, padding="same"))
+        # model.add(LeakyReLU(alpha=0.2))
+        # model.add(Dropout(0.25))
+        # model.add(Conv2D(32, kernel_size=3, strides=2, padding="same"))
+        # model.add(ZeroPadding2D(padding=((0, 1), (0, 1))))
+        # model.add(BatchNormalization(momentum=0.8))
+        # model.add(LeakyReLU(alpha=0.2))
+        # model.add(Dropout(0.25))
+        # model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
+        # model.add(BatchNormalization(momentum=0.8))
+        # model.add(LeakyReLU(alpha=0.2))
+        # model.add(Dropout(0.25))
+        # model.add(Conv2D(128, kernel_size=3, strides=1, padding="same"))
+        # model.add(BatchNormalization(momentum=0.8))
+        # model.add(LeakyReLU(alpha=0.2))
+        # model.add(Dropout(0.25))
+        # model.add(Flatten())
+        # model.add(Dense(1))
+
+        model.add(Conv2D(256, kernel_size=(3, 64), input_shape=self.sentence_shape))
         model.add(LeakyReLU(alpha=0.2))
-        model.add(Dropout(0.25))
-        model.add(Conv2D(32, kernel_size=3, strides=2, padding="same"))
-        model.add(ZeroPadding2D(padding=((0, 1), (0, 1))))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dropout(0.25))
-        model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dropout(0.25))
-        model.add(Conv2D(128, kernel_size=3, strides=1, padding="same"))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dropout(0.25))
+        model.add(Conv2D(128, kernel_size=(5, 1)))
         model.add(Flatten())
-        model.add(Dense(1))
+        model.add(Dense(1, activation='sigmoid'))
 
         model.summary()
-        model.save("WGAN2vec_discriminator.h5")
+        # model.save("WGAN2vec_discriminator.h5")
 
-        img = Input(shape=self.img_shape)
+        img = Input(shape=self.sentence_shape)
         validity = model(img)
 
         return Model(img, validity)
 
     def pretrain_D(self, epochs, batch_size=128):
         X_train = sentence_to_word_vec
-        valid = np.ones((batch_size, 1))
+        # valid = np.ones((batch_size, 1))
+        valid = -np.ones((batch_size, 1))
         fake = np.zeros((batch_size, 1))
         valid = valid * 0.9
         # fake = fake + 0.1
@@ -163,17 +188,21 @@ class WGAN2vec():
             gen_sentences = self.generator.predict(noise)
 
             # Train the discriminator (real classified as ones and generated as zeros)
-            d_loss_real = self.discriminator.train_on_batch(sentences, valid)
-            d_loss_fake = self.discriminator.train_on_batch(gen_sentences, fake)
-            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+            # d_loss_real = self.discriminator.train_on_batch(sentences, valid)
+            # d_loss_fake = self.discriminator.train_on_batch(gen_sentences, fake)
+            d_loss_real = self.critic.train_on_batch(sentences, valid)
+            d_loss_fake = self.critic.train_on_batch(gen_sentences, fake)
+            # d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+            d_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
 
-    def train(self, epochs, batch_size=128, save_interval=50):
+    def train(self, epochs, batch_size=128, sample_interval=50):
 
         # Load the dataset
         X_train = sentence_to_word_vec
 
         # Adversarial ground truths
-        valid = np.ones((batch_size, 1))
+        # valid = np.ones((batch_size, 1))
+        valid = -np.ones((batch_size, 1))
         fake = np.zeros((batch_size, 1))
         valid = valid * 0.9
         # fake = fake + 0.1
@@ -185,16 +214,25 @@ class WGAN2vec():
 
             # Select a random half of images
             idx = np.random.randint(0, X_train.shape[0], batch_size)
-            imgs = X_train[idx]
+            sentences = X_train[idx]
 
             # Sample noise and generate a batch of new images
             noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
-            gen_imgs = self.generator.predict(noise)
+            gen_sentences = self.generator.predict(noise)
 
             # Train the discriminator (real classified as ones and generated as zeros)
-            d_loss_real = self.discriminator.train_on_batch(imgs, valid)
-            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
-            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+            # d_loss_real = self.discriminator.train_on_batch(imgs, valid)
+            # d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
+            d_loss_real = self.critic.train_on_batch(sentences, valid)
+            d_loss_fake = self.critic.train_on_batch(gen_sentences, fake)
+            # d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+            d_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
+
+            # Clip critic weights
+            for l in self.critic.layers:
+                weights = l.get_weights()
+                weights = [np.clip(w, -self.clip_value, self.clip_value) for w in weights]
+                l.set_weights(weights)
 
             # ---------------------
             #  Train Generator
@@ -204,10 +242,11 @@ class WGAN2vec():
             g_loss = self.combined.train_on_batch(noise, valid)
 
             # Plot the progress
-            print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100 * d_loss[1], g_loss))
+            # print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100 * d_loss[1], g_loss))
+            print("%d [D loss: %f] [G loss: %f]" % (epoch, 1 - d_loss[0], 1 - g_loss[0]))
 
             # If at save interval => save generated image samples
-            if epoch % save_interval == 0:
+            if epoch % sample_interval == 0:
                 self.show_sentence(epoch)
 
     def show_sentence(self, epoch):
